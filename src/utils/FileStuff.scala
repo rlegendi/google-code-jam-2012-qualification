6df60c5855c6b22cc092e8301ffdec5937df7e92
Richard O. Legendi <richard.legendi@gmail.com>
2012-04-24 18:17:09 +0200

    Added Dancing with the Googlers solution (quite noisy)

---
+++ src/utils/FileStuff.scala
@@ -0,0 +1,32 @@
+package utils
+import java.io.PrintWriter
+
+object FileStuff {
+  def readStuff(fname: String, maxTestCases: Int): List[String] = {
+    val source = scala.io.Source.fromFile(fname)
+    val allLines = source.getLines.toList
+
+    val dataLength = allLines.first.toInt
+
+    if (dataLength < 1 || maxTestCases < dataLength) {
+      throw new IllegalArgumentException("Data length must be in interval [1, 30], was " + dataLength)
+    }
+
+    // HELPME If I write tail() here, it is erroneous
+    val data = allLines.tail
+
+    // HELPME But! Here I can write it...
+    if (dataLength != data.size) {
+      throw new IllegalArgumentException("Data length does not fit lines in file; dataLength=" + dataLength + ", data.length=" + data.length)
+    }
+
+    source.close
+    data
+  }
+
+  def writeStuff(fname: String, data: List[String]) = {
+    val fw = new PrintWriter(fname)
+    data.foreach(fw.println(_))
+    fw.close()
+  }
+}
