c0dac2e05d88fa379d949e930b6051c9a827c623
Richard O. Legendi <richard.legendi@gmail.com>
2012-04-24 18:17:31 +0200

    Added Spec2 tests and updated README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 http://code.google.com/codejam/contest/dashboard?c=1460488#s=p1
+
+# Scala-Related Questions #
+
+What is the point of having both `length()` and `size()`?
+
+```scala
+println(List("A").length)
+println(List("A").size)
+```
+
+# For the Typesafe Guys #
+
+A few things I spotted in the Scala IDE.
+
+  // FIXME Quicfix: create def it is unknown, like here if it does not exists
+  // lines.foreach( decode(_) )
+
+  // FIXME Show error text not on just the Markers, but the error too. E.g.,
+  // lines.foreach( println(decode(_)) )
+
+  // FIXME Bolding TODO/FIXME/etc.
+  // FIXME Why is the indentation 2 spaces by default?
+  
+
+Hotkeys for Ctrl + Shift + N on the project explorer.
+
+# IDEAS #
+
+## Specs2 ##
+
+* Syntax highlight for should/in/must/etc.
---
+++ test/b/ScoreTest.scala
@@ -0,0 +1,125 @@
+package b
+
+import org.specs2.mutable.Specification
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ScoreTest extends Specification {
+  "For the given example, class Score.hasScore " should {
+    "sum score 13 must not contain a score of 3" in {
+      Score.hasScore(13, 3) must beTrue
+    }
+
+    "sum score 13 must contain a score of 4" in {
+      Score.hasScore(13, 4) must beTrue
+    }
+
+    "sum score 13 must contain a score of 5" in {
+      Score.hasScore(13, 5) must beTrue
+    }
+
+    "sum score 13 must not contain a score of 6" in {
+      Score.hasScore(13, 6) must beFalse
+    }
+
+    "sum score 14 must not contain a score of 3" in {
+      Score.hasScore(14, 3) must beTrue
+    }
+
+    "sum score 14 must contain a score of 4" in {
+      Score.hasScore(14, 4) must beTrue
+    }
+
+    "sum score 14 must contain a score of 5" in {
+      Score.hasScore(14, 5) must beTrue
+    }
+
+    "sum score 14 must not contain a score of 6" in {
+      Score.hasScore(14, 6) must beFalse
+    }
+
+    "sum score 15 must not contain a score of 4" in {
+      Score.hasScore(15, 4) must beTrue
+    }
+
+    "sum score 15 must contain a score of 5" in {
+      Score.hasScore(15, 5) must beTrue
+    }
+
+    "sum score 15 must not contain a score of 6" in {
+      Score.hasScore(15, 6) must beFalse
+    }
+  }
+
+  "For the given example, class Score.mightHaveExtendedScore " should {
+
+    "surprising sum score 13 must not contain a score of 3" in {
+      Score.mightHaveExtendedScore(13, 2) must beFalse
+    }
+
+    "surprising sum score 13 must contain a score of 3" in {
+      Score.mightHaveExtendedScore(13, 3) must beTrue
+    }
+
+    "surprising sum score 13 must contain a score of 6" in {
+      Score.mightHaveExtendedScore(13, 6) must beFalse // 4 4 5 --> 3 5 5 max.
+    }
+
+    "surprising sum score 13 must not contain a score of 7" in {
+      Score.mightHaveExtendedScore(13, 7) must beFalse
+    }
+
+    "surprising sum score 14 must not contain a score of 2" in {
+      Score.mightHaveExtendedScore(14, 2) must beFalse
+    }
+
+    //    "surprising sum score 14 must contain a score of 3" in {
+    //      Score.mightHaveExtendedScore(14, 3) must beTrue
+    //    }
+
+    "surprising sum score 14 must contain a score of 6" in {
+      Score.mightHaveExtendedScore(14, 6) must beTrue
+    }
+
+    "surprising sum score 14 must not contain a score of 7" in {
+      Score.mightHaveExtendedScore(14, 7) must beFalse
+    }
+
+    "surprising sum score 15 must not contain a score of 3" in {
+      Score.mightHaveExtendedScore(15, 3) must beFalse
+    }
+
+    "surprising sum score 15 must contain a score of 4" in {
+      Score.mightHaveExtendedScore(15, 4) must beTrue
+    }
+
+    "surprising sum score 15 must contain a score of 6" in {
+      Score.mightHaveExtendedScore(15, 6) must beTrue
+    }
+
+    "surprising sum score 15 must not contain a score of 7" in {
+      Score.mightHaveExtendedScore(15, 7) must beFalse
+    }
+  }
+
+  "For the given example series, Series" should {
+    "#1 result in 3" in {
+      Score.evalSeq(3, 1, 5, List(15, 13, 11)) must be_==(3)
+    }
+
+    "#2 result in 2" in {
+      Score.evalSeq(3, 0, 8, List(23, 22, 21)) must be_==(2)
+      // 7 7 8 (1)
+      // 7 8 8 (2)
+    }
+
+    "#3 result in 1" in {
+      Score.evalSeq(2, 1, 1, List(8, 0)) must be_==(1)
+    }
+
+    "#4 result in 3" in {
+      Score.evalSeq(6, 2, 8, List(29, 20, 8, 18, 18, 21)) must be_==(3)
+    }
+  }
+}
